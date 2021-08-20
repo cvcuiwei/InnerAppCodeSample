da18187d40ea2ecca0d26b534a5512dc86e089a0
caiozhang <caiozhang@tencent.com>
2021-08-20 22:04:04 +0800

    feat: demo框架搭建

--- vue_dev/src/page/contextManage/index.vue
+++ vue_dev/src/page/contextManage/index.vue
@@ -1,13 +1,33 @@
 <template>
     <div>
-        hello world
+        <Button @click="onClick">测试</Button>
+        <div>{{callback}}</div>
     </div>
 </template>
 <script>
+import {Button} from 'element-ui';
+import {get} from 'axios';
 export default {
     name: 'contextManage',
+    components: {
+        Button
+    },
     data() {
-        return {}
-    }
+        return {
+            callback: ''
+        }
+    },
+    methods: {
+        async onClick() {
+            const { data } = await get('/api/user/get', {
+                params: {
+                    userid: 'haluo',
+                    corpid: 'ww5d12b2a998ef967c',
+                    corpsecret: '***REMOVED***',
+                }
+            });
+            this.callback = JSON.stringify(data);
+        }
+    },
 }
 </script>
